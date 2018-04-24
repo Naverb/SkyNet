52431aa8f3e3564a2b262326a0dddabad2a865a4
Naverb <brevanellefsen@gmail.com>
2018-04-23 22:08:38 -0600

    hotfix 2

--- MWP/TreeFarm.lua
+++ MWP/TreeFarm.lua
@@ -49,6 +49,7 @@ while true do
         turtle.forward()
     elseif (data.name == "minecraft:log") then
         turtle.dig()
+        turle.forward()
         turtle.digUp()
         turtle.up()
         turtle.select(2)
