b486e676ae1fbfa710416449bd4b7f67a8be8b09
Naverb <brevanellefsen@gmail.com>
2018-04-28 01:02:40 -0600

    added quarry code

---
+++ MWP/quarry.lua
@@ -0,0 +1,22 @@
+os.loadAPI("SkyNet/MWP/lib/quarry_api")
+
+gps2.orientChunk()
+
+
+
+turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+
+    for i=1,8 do   
+        quarry_api.quarryLine()
+        turtle.turnRight()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnRight()
+        quarry_api.quarryLine()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnLeft()
+    end
