3a25e108fcc06795356f2ca997fa97063e35ee30
Naverb <brevanellefsen@gmail.com>
2018-04-28 01:14:29 -0600

    actually feed quarry code a height

--- MWP/quarry.lua
+++ MWP/quarry.lua
@@ -1,14 +1,14 @@
 os.loadAPI("SkyNet/MWP/lib/quarry_api")
 
-gps2.orientChunk()
+local height = gps2.orientChunk()
 
 for i=1,8 do   
-    quarry_api.quarryLine()
+    quarry_api.quarryLine(height)
     turtle.turnRight()
     turtle.dig()
     turtle.forward()
     turtle.turnRight()
-    quarry_api.quarryLine()
+    quarry_api.quarryLine(height)
     turtle.turnLeft()
     turtle.dig()
     turtle.forward()
