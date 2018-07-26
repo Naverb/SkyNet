aa8e1e87925ec62a6314e76e1880c72df2f9defe
Naverb <brevanellefsen@gmail.com>
2018-07-25 20:41:13 -0600

    I should push

---
+++ MWP/lib/log.lua
@@ -0,0 +1,7 @@
+function log(params)
+	-- settings
+	local filePath="/Logfile.txt"
+		-- This is the destination where the logs and errors in writed.
+	local doLogFile = true
+		-- To turn of the log file, change this to 'false'.
+end
