370d1f262a008bf10f12857633a3409484edbc38
Anthony D'Arienzo <anthony.dev@outlook.com>
2019-03-20 17:05:41 -0400

    Adding code to print stdout to logfile.

--- MWP/lib/core/log.lua
+++ MWP/lib/core/log.lua
@@ -2,28 +2,6 @@ local LOG_DIR = '/MWP/sys/log'
 local CURRENT_LOG_DIR = fs.combine(LOG_DIR,'recent.log')
 local current_log_number = 0
 local logfiles = { MAIN = 'recent.log'}
-function initialize()
-	-- Create a new log file, renaming the last one.
-	-- We first rename the previous log file:
-	if fs.exists(CURRENT_LOG_DIR) then
-		local last_log = fs.open(CURRENT_LOG_DIR,'r')
-		local header = last_log.readLine()
-		last_log.close()
-		current_log_number = string.match(header,'%d+') + 1
-		-- We rename the log file here:
-		print('Current log number: ' .. current_log_number)
-		fs.move(CURRENT_LOG_DIR,fs.combine(LOG_DIR,'log_' .. tostring(current_log_number - 1)) )
-	else
-		-- There is no most-recent log file. We can just create one.
-		current_log_number = current_log_number + 1
-		print('Current log number: ' .. current_log_number)
-	end
-
-	-- We now create the new log file
-	local logfile = fs.open(CURRENT_LOG_DIR,'w')
-	logfile.writeLine('=== SKYNET LOG FILE: ' .. tostring(current_log_number) .. ' ===')
-	logfile.close()
-end
 
 function createLogFile(name,path)
 	-- Create a new log file with the given name and path (relative to LOG_DIR)
@@ -39,19 +17,84 @@ function createLogFile(name,path)
 	file.close()
 	logfiles[name] = path
 end
-
-function printToLog(msg,logfile)
+function writeToLog(msg,logfile)
 	logfile = logfile or 'MAIN'
 	-- Writes the string msg to the specified logfile
 	local logpath = fs.combine(LOG_DIR,logfiles[logfile])
 	local file = fs.open(logpath,'a')
-	file.writeLine(msg)
+	file.write(msg)
 	file.close()
+end
+function printToLog(msg,logfile)
+	writeToLog(msg .. '\n',logfile)
+end
+function initialize()
+	-- Create a new log file, renaming the last one.
+	-- We first rename the previous log file:
+	if fs.exists(CURRENT_LOG_DIR) then
+		local last_log = fs.open(CURRENT_LOG_DIR,'r')
+		local header = last_log.readLine()
+		last_log.close()
+		current_log_number = (string.match(header,'%d+') or 0) + 1 -- Just in case the header is missing, start back at 0.
+		-- We rename the log file here:
+		print('Current log number: ' .. current_log_number)
+		fs.move(CURRENT_LOG_DIR,fs.combine(LOG_DIR,'log_' .. tostring(current_log_number - 1)) )
+	else
+		-- There is no most-recent log file. We can just create one.
+		current_log_number = current_log_number + 1
+		print('Current log number: ' .. current_log_number)
+	end
+
+	-- We now create the new log file
+	local logfile = fs.open(CURRENT_LOG_DIR,'w')
+	logfile.writeLine('=== SKYNET LOG FILE: ' .. tostring(current_log_number) .. ' ===')
+	logfile.close()
+
+	-- We now overwrite the print function to incorporate logging:
+	-- We do this in initialize because we want to make sure we have a logfile before we start using it.
+	print = function(...)
+		-- We steal part of the built-in print function to format properly:
+
+		-- We also overwrite the original write function in this scope to write to the log file:
+
+		local lua_write = write
+		local write = function(str)
+
+			local serialized_str
+			-- Very basic serialization code:
+			if type(str) == 'table' then
+				local ok, serialized_str = pcall(textutils.serialize,str)
+				if not ok then
+					serialized_str = tostring(str)
+				elseif serialized_str == "" or serialized_str == '{}' then
+					serialized_str = "[EMPTY_TABLE]"
+				end
+			else
+				serialized_str = tostring(str)
+			end
+
+			writeToLog(serialized_str)
+			return write(str)
+		end
+
+		local nLinesPrinted = 0
+		local nLimit = select("#", ... )
+		for n = 1, nLimit do
+			local s = tostring( select( n, ... ) )
+			if n < nLimit then
+				s = s .. "\t"
+			end
+			nLinesPrinted = nLinesPrinted + write( s )
+		end
+		nLinesPrinted = nLinesPrinted + write( "\n" )
+		return nLinesPrinted
+	end
 end
 
 -- We have this here just in case we load this API before module.
 return {
 	initialize = initialize,
 	createLogFile = createLogFile,
-	print = printToLog
+	print = printToLog,
+	write = writeToLog
 }
